48436bd8fa8ab4868f3e4db0cfaabf21902a857e
katiakniazhevich <55781001+katiakniazhevich@users.noreply.github.com>
2021-03-15 22:18:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n *= -1;
+        n = n + "";
+	return n.split("").reverse().join("");
+    }else{
+    n = n + "";
+	return n.split("").reverse().join("");
+    }
 }
